c95a65ba1a33dfa7c8fcd7dd0cf2badffa9b008d
Nick Apostolakis <nickapos@oncrete.gr>
2016-07-17 20:15:11 +0100

    adding list filters that allow us to filter inclusively or exclusively

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 parseHosts=parseHostsModule
 parseHosts: ; ocamlfind ocamlc -package extlib -linkpkg -I $(parseHosts)/ -o parseHosts $(parseHosts)/parseHosts.ml
-parseHostsLine: ; ocamlfind ocamlc -package batteries -package extlib -linkpkg -I $(parseHosts)/ -o parseHostsLine $(parseHosts)/parseHostsLIne.ml
 
 .PHONY: clean all
 
 clean: ; rm $(parseHosts)/*.cm* parseHosts *log *cache
-all: parseHosts parseHostsLine
+all: parseHosts 
 
--- parseHostsModule/parseHosts.ml
+++ parseHostsModule/parseHosts.ml
@@ -1,3 +1,4 @@
+open ExtLib
 class parseHosts = object (self)
  (*
   A class that will open /etc/hosts and parse its content
@@ -13,6 +14,9 @@ class parseHosts = object (self)
     let chan = open_in "/etc/hosts" in
         Std.input_list chan
 
+  (*
+   *This method will print the contents of a list line by line
+   *)
   method printHostsContents lines = match lines with
       | [] -> self#printNL "End of file" 
       | x::xs -> 
@@ -20,7 +24,19 @@ class parseHosts = object (self)
             self#printNL x;
             self#printHostsContents xs
           end
-    
+  (*
+   *This method will return a list with lines that contain the string of the
+   filter
+   *)
+  method includeListLine incomingList filterStr=
+    List.filter (fun x -> String.exists x filterStr) incomingList
+  
+  (*
+   *This method will return a list with lines that does not contain the string of the
+   filter
+   *)
+  method excludeListLine incomingList filterStr=
+    List.filter (fun x -> not (String.exists x filterStr)) incomingList
 
 end;;
 
@@ -30,7 +46,7 @@ let main () =
           Array.iter cat argv *)
           (* create an object*)
           let obj = new parseHosts in
-            obj#printHostsContents obj#readHosts
+            obj#printHostsContents (obj#excludeListLine obj#readHosts "localhost")
 
 
 let _ = main ()
