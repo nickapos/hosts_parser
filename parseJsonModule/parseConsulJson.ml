ad18f872d23091d3669bb5c58464e7bc26d20457
Your Name <you@example.com>
2016-07-19 20:40:03 +0000

    adding http client capabilities

--- parseJsonModule/parseConsulJson.ml
+++ parseJsonModule/parseConsulJson.ml
@@ -1,13 +1,26 @@
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
 class parseConsulJson = object (self)
   inherit ParseJsonBase.parseJsonBase
  (*
   A class that will be able to parse a json file from consul
   *) 
 
+  method getConsul username password endpoint =
+    let urlString=Printf.sprintf "http://%s:%s@consul.oncrete.gr/v1/kv/%s/externalIP" username password endpoint in
+    Client.get (Uri.of_string urlString) >>= fun (resp, body) ->
+    let code = resp |> Response.status |> Code.code_of_status in
+    Printf.printf "Response code: %d\n" code;
+    Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+    body |> Cohttp_lwt_body.to_string >|= fun body ->
+    Printf.printf "Body of length: %d\n" (String.length body);
+    body
+
 
 end;;
 
-let main () =
+let _ =
       let len = (Array.length Sys.argv) in
         let argv = (Array.sub Sys.argv 1 (len-1)) in 
           (*Array.iter cat argv *)
@@ -16,10 +29,9 @@ let main () =
            * hostname, third the new ip
            *)
           let obj = new parseConsulJson in
-            let json = obj#readJsonFromFile "example.json" in 
-            let jsonStr= obj#getJsonElement "Value" json in
-            obj#printNL (obj#decodeBase64 jsonStr)
+            let endpointList = ["blackwood";"mnisikli"] in 
+            let f elem= obj#printNL (Lwt_main.run (obj#getConsul username password elem)) in
+              List.iter f endpointList
 
 
-let _ = main ()
 
